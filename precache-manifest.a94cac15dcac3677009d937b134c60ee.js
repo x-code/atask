a247504d41be90bf5e1a82335175c92d9c691f58
Rian Firandika <rian.firandika@aido.id>
2023-07-05 02:04:32 +0700

    Updates

---
+++ precache-manifest.a94cac15dcac3677009d937b134c60ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b928260bd08274ae612d80642b07080a",
+    "url": "/atask/index.html"
+  },
+  {
+    "revision": "5114bc5636551f026652",
+    "url": "/atask/static/css/main.becb366f.chunk.css"
+  },
+  {
+    "revision": "db0452500dc6e06d07c1",
+    "url": "/atask/static/js/2.283c07d2.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/atask/static/js/2.283c07d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5114bc5636551f026652",
+    "url": "/atask/static/js/main.c0df1f1d.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/atask/static/js/main.c0df1f1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c39966ed165475e5cfbd",
+    "url": "/atask/static/js/runtime-main.960c8d38.js"
+  }
+]);
